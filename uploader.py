445cb9e189f49335034addbe3d7c57c3c7cce0f3
Unknown <azhang042@gmail.com>
2016-11-09 09:57:55 -0800

    FIxed uploader credit output

--- uploader.py
+++ uploader.py
@@ -17,9 +17,10 @@ accessToken = "4c80c2924ddeb63d3f1c99d19ae04e01e438b5fb"
 global PATHS
 PATHS = {}
 
-print(imgur.credits)
-imgur = ImgurClient("5e1b2fcfcf0f36e", "d919f14c31fa97819b1e9c82e2be40aef8bd9682", accessToken, refreshToken)
 
+imgur = ImgurClient("5e1b2fcfcf0f36e", 
+"d919f14c31fa97819b1e9c82e2be40aef8bd9682", accessToken, refreshToken)
+print(imgur.credits)
 
 print("IMGUR INIT")
 with open("paths.txt", "r") as f:
