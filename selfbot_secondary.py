62e8223d001b62fc5aa2e77c3e59593ee6da990f
Zenith00 <z@zenith.dev>
2016-12-26 19:45:09 -0800

    Added toggling of art post bot

--- selfbot_secondary.py
+++ selfbot_secondary.py
@@ -4,12 +4,16 @@ import logging
 import constants
 from simplegist.simplegist import Simplegist
 from datetime import datetime
-
+from utils_text import *
 from TOKENS import *
+from duration_timer import timer
+from utils_file import *
 client = discord.Client()
 logging.basicConfig(level=logging.INFO)
 gistClient = Simplegist()
 
+art_timer = timer(1800)
+art_on = False
 @client.event
 async def on_ready():
     print('Connected!')
@@ -18,20 +22,31 @@ async def on_ready():
 
 @client.event
 async def on_message(message):
+    global art_on
     if message.author == client.user:
         if message.content.startswith("]]"):
             command = message.content.replace("]]", "")
             await client.delete_message(message)
-
+            if command == "art":
+                await client.send_message(client.get_server("262761876373372938"), "Toggling artbot from {} to {}".format(art_on, not art_on))
+                art_on = not art_on
             if command == "1":
                 modlist = await get_moderators(message.server)
-                gist = gistClient.create(name="Mods of " + message.server.name,
-                                         description=str(datetime.utcnow().strftime("[%Y-%m-%d %H:%m:%S] ")),
+                infodump = []
+                for mod in modlist:
+                    infodump.append([ascii(mod.name), mod.id])
+                info = await pretty_column(infodump, True)
+                print(str(info))
+                # info = info.replace("'","")
+                gist = gistClient.create(name="Modlist", description=message.server.name + " moderators",
                                          public=False,
-                                         content=str(modlist))
+                                         content=info)
                 await client.send_message(client.get_server(constants.OVERWATCH_SERVER_ID).get_member(constants.ZENITH_ID), gist["Gist-Link"])
 
-
+    if art_timer.is_next() and art_on:
+        file = extract_line(artlist)
+        if file != "":
+            await client.send_message(client.get_server(constants.OVERWATCH_SERVER_ID).get_channel("168567769573490688"), file)
 async def get_moderators(server):
     users = []
     for role in server.roles:
