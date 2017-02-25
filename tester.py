6f86f9b835894802d249600ca9f3e673820e4833
Zenith00 <z@zenith.dev>
2017-02-25 10:09:28 -0800

    Commit

--- tester.py
+++ tester.py
@@ -1,11 +1,65 @@
-from datetime import datetime, timedelta
-import time
-
-saved_time = datetime.now()
-time.sleep(5)
-print((datetime.now() - saved_time).total_seconds())
-
-
-#  import sys
-# numbers = input().split(" ")
-# print(numbers[-1].join(numbers[:-1]) + " = " + str(int(numbers[0]) + sum([int(x) for x in numbers[1:-1]]) if numbers[-1] == "+" else int(numbers[0]) - sum([int(x) for x in numbers[1:-1]])))
+{"meta"    : {"generated_at": "2017-02-25T17:15:04Z"}, "template": {"small" : "https:\/\/static-cdn.jtvnw.net\/emoticons\/v1\/{image_id}\/1.0",
+                                                                    "medium": "https:\/\/static-cdn.jtvnw.net\/emoticons\/v1\/{image_id}\/2.0",
+                                                                    "large" : "https:\/\/static-cdn.jtvnw.net\/emoticons\/v1\/{image_id}\/3.0"}, "channels": {
+    "--twitch-curse-integration--"                                                                           : {"title"     : "Twitch - Curse Integration",
+                                                                                                                "channel_id": None,
+                                                                                                                "link"      : "http:\/\/twitch.tv\/--twitch-curse-integration--",
+                                                                                                                "desc"      : None,
+                                                                                                                "id"        : "--twitch-curse-integration--",
+                                                                                                                "first_seen": "2016-09-26 19:05:04",
+                                                                                                                "badge"     : None, "badge_starting": None,
+                                                                                                                "badge_3m"  : None, "badge_6m": None,
+                                                                                                                "badge_12m" : None, "badge_24m": None,
+                                                                                                                "set"       : 19151, "emotes": [
+            {"code": "TwitchLit", "image_id": 115390}]}, "--twitch-turbo--"                                  : {"title"         : "Twitch Turbo and Prime",
+                                                                                                                "channel_id"    : None,
+                                                                                                                "link"          : "https:\/\/secure.twitch.tv\/products\/turbo\/ticket",
+                                                                                                                "desc"          : None,
+                                                                                                                "id"            : "--twitch-turbo--",
+                                                                                                                "first_seen"    : None, "badge": None,
+                                                                                                                "badge_starting": None, "badge_3m": None,
+                                                                                                                "badge_6m"      : None, "badge_12m": None,
+                                                                                                                "badge_24m"     : None, "set": 457, "emotes": [
+            {"code": "KappaHD", "image_id": 2867}, {"code": "MiniK", "image_id": 2868}, {"code": "ScaredyCat", "image_id": 112296},
+            {"code": "TableHere", "image_id": 112297}, {"code": "FlipThis", "image_id": 112298}]}, "00_keoma": {"title"         : "00_Keoma",
+                                                                                                                "channel_id"    : 24590287,
+                                                                                                                "link"          : "http:\/\/twitch.tv\/00_keoma",
+                                                                                                                "desc"          : None, "id": "00_keoma",
+                                                                                                                "first_seen"    : "2016-04-15 10:35:03",
+                                                                                                                "badge"         : "https:\/\/static-cdn.jtvnw.net\/badges\/v1\/06703249-99f6-4e34-a33c-423b10d88893\/1",
+                                                                                                                "badge_starting": "https:\/\/static-cdn.jtvnw.net\/badges\/v1\/06703249-99f6-4e34-a33c-423b10d88893\/3",
+                                                                                                                "badge_3m"      : None, "badge_6m": None,
+                                                                                                                "badge_12m"     : None, "badge_24m": None,
+                                                                                                                "set"           : 16726, "emotes": [
+            {"code": "slsParrot", "image_id": 94961}]}, "0kr0shka"                                           : {"title"         : "0kr0shka",
+                                                                                                                "channel_id"    : 29225105,
+                                                                                                                "link"          : "http:\/\/twitch.tv\/0kr0shka",
+                                                                                                                "desc"          : None, "id": "0kr0shka",
+                                                                                                                "first_seen"    : None,
+                                                                                                                "badge"         : "https:\/\/static-cdn.jtvnw.net\/badges\/v1\/40982b33-1b71-4cc5-b495-9c5e349ccb2c\/1",
+                                                                                                                "badge_starting": "https:\/\/static-cdn.jtvnw.net\/badges\/v1\/40982b33-1b71-4cc5-b495-9c5e349ccb2c\/3",
+                                                                                                                "badge_3m"      : None, "badge_6m": None,
+                                                                                                                "badge_12m"     : None, "badge_24m": None,
+                                                                                                                "set"           : 5181, "emotes": [
+            {"code": "0krXorya", "image_id": 15889}, {"code": "0kr0kr0shka", "image_id": 15925}]}, "00mao00" : {"title"         : "\u6bdb",
+                                                                                                                "channel_id"    : 18808579,
+                                                                                                                "link"          : "http:\/\/twitch.tv\/00mao00",
+                                                                                                                "desc"          : None, "id": "00mao00",
+                                                                                                                "first_seen"    : None,
+                                                                                                                "badge"         : "https:\/\/static-cdn.jtvnw.net\/badges\/v1\/d4f76269-3bcf-4b40-b6ed-79cbca415cee\/1",
+                                                                                                                "badge_starting": "https:\/\/static-cdn.jtvnw.net\/badges\/v1\/d4f76269-3bcf-4b40-b6ed-79cbca415cee\/3",
+                                                                                                                "badge_3m"      : None, "badge_6m": None,
+                                                                                                                "badge_12m"     : None, "badge_24m": None,
+                                                                                                                "set"           : 11954, "emotes": [
+            {"code": "galalaSB", "image_id": 40398}]}, "0nuqtive"                                            : {"title"         : "0nuqtive",
+                                                                                                                "channel_id"    : 32025368,
+                                                                                                                "link"          : "http:\/\/twitch.tv\/0nuqtive",
+                                                                                                                "desc"          : None, "id": "0nuqtive",
+                                                                                                                "first_seen"    : "2016-06-10 14:30:12",
+                                                                                                                "badge"         : "https:\/\/static-cdn.jtvnw.net\/badges\/v1\/7c9b4c91-3ed3-4f04-98ed-3d4627ef4a26\/1",
+                                                                                                                "badge_starting": "https:\/\/static-cdn.jtvnw.net\/badges\/v1\/7c9b4c91-3ed3-4f04-98ed-3d4627ef4a26\/3",
+                                                                                                                "badge_3m"      : "https:\/\/static-cdn.jtvnw.net\/badges\/v1\/a8d4870d-87cc-475e-9c31-23661ea5159d\/3",
+                                                                                                                "badge_6m"      : "https:\/\/static-cdn.jtvnw.net\/badges\/v1\/8f807873-92c8-4cac-b93b-a1fbdf747806\/3",
+                                                                                                                "badge_12m"     : None, "badge_24m": None,
+                                                                                                                "set"           : 17769, "emotes": [
+            {"code": "onuqFail", "image_id": 102317}]
