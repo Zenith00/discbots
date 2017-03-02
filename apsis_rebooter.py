bdc0bb4800e390c048c5b247c5e7b5d5c892493e
Zenith00 <z@zenith.dev>
2017-03-01 17:10:10 -0800

    Commit

--- apsis_rebooter.py
+++
@@ -1,9 +0,0 @@
-import ast
-import time
-import subprocess
-
-
-while True:
-    print("REBOOTING")
-    p = subprocess.call(["python.exe",r"C:\Users\Austin\Desktop\Programming\Disc\selfbot.py"], creationflags=subprocess.CREATE_NEW_CONSOLE)
-    time.sleep(5)
