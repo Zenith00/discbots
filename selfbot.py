974bccc3658a871bca8312c76ca8c2d6e171a433
Zenith00 <z@zenith.dev>
2017-03-21 20:47:48 -0700

    Get ava

--- selfbot.py
+++ selfbot.py
@@ -77,10 +77,12 @@ async def on_message(message_in):
                 await client.edit_profile(password=PASS, avatar=ava.read())
         if command_list[0] == "stealava":
             command_list = await mention_to_id(command_list)
-            url = message_in.server.get_member(command_list[1]).avatar_url
+            target_user_id = command_list[1]
+            print(target_user_id)
+            url = message_in.server.get_member(target_user_id).avatar_url
             response = requests.get(url)
             img = Image.open(BytesIO(response.content))
-            img_path = utils_file.relative_path(__file__, "avatars/" + command_list[1] + ".png")
+            img_path = utils_file.relative_path(__file__, "avatars/" + target_user_id + ".png")
             # if os.path.isfile(img_path):
             #     os.remove(img_path)
             img.save(img_path, 'PNG')
