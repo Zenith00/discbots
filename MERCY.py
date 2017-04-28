ef3815eda296b00068b14882584f46fc3166531e
Zenith00 <z@zenith.dev>
2017-04-27 23:54:05 -0700

    update

--- MERCY.py
+++ MERCY.py
@@ -437,7 +437,7 @@ async def on_message(message_in):
     # global PATHS
     # global ENABLED
     # global INITIALIZED
-    if message_in.server.id == "266279338305912832" and message_in.author.id == "183083101436641280":
+    if message_in.server.id == "266279338305912832" and message_in.author.top_role == await get_role(message_in.server, "307400427954110465"):
         print(int("#%06x" % random.randint(0, 0xFFFFFF), 16))
         await client.edit_role(message_in.server, await get_role(message_in.server, "307400427954110465"), color=discord.Color(int("%06x" % random.randint(0, 0xFFFFFF), 16)))
     if not STATES["init"]:
