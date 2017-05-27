9cb3784a0dc3ac74a6529fe868f8ba1bfd4c7ad7
Zenith00 <z@zenith.dev>
2017-05-26 23:02:21 -0700

    Update

--- selfbot.py
+++ selfbot.py
@@ -165,22 +165,7 @@ async def on_message(message_in):
                     start = start + timedelta(days=30)
                     end = end + timedelta(days=30)
 
-                # cursor = overwatch_db.message_log.aggregate(
-                #     [
-                #         {"$group": {"_id": "$message_id", "unique_ids": {"$addToSet": "$_id"}, "count": {"$sum": 1}}},
-                #         {"$match": {"count": {"$gte": 2}}}
-                #     ]
-                # , allowDiskUse = True )
-                # response = []
-                # count = 0
-                # async for doc in cursor:
-                #
-                #     print(count)
-                #     count = count + 1
-                #     del doc["unique_ids"][0]
-                #     for id in doc["unique_ids"]:
-                #         response.append(id)
-                # await overwatch_db.message_log.remove({"_id": {"$in": response}})
+
 
             if command_list[0] == "find":
                 # await output_find_user(message_in)
