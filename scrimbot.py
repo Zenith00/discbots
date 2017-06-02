a20e936a8535a173710fc162d442cf1773c6bf3a
Zenith00 <z@zenith.dev>
2017-06-02 10:47:39 -0700

    Update

--- scrimbot.py
+++ scrimbot.py
@@ -10,7 +10,20 @@ mongo_client = motor.motor_asyncio.AsyncIOMotorClient()
 client = discord.Client()
 os.environ["PYTHONUNBUFFERED"] = "True"
 sys.stdout = os.fdopen(sys.stdout.fileno(), 'w', 1)
+class Unbuffered(object):
+    def __init__(self, stream):
+        self.stream = stream
 
+    def write(self, data):
+        self.stream.write(data)
+        self.stream.flush()
+
+    def __getattr__(self, attr):
+        return getattr(self.stream, attr)
+
+import sys
+
+sys.stdout = Unbuffered(sys.stdout)
 event_dict = {}
 
 
