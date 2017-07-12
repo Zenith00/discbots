e7357c3cfd4e385d12273ff0effa1ed032eb4eff
Zenith00 <z@zenith.dev>
2017-07-11 21:40:10 -0700

    Restrict linkbot to only work with 334043962094387201

--- linkbot.py
+++ linkbot.py
@@ -7,8 +7,9 @@ client = discord.Client()
 
 @client.event
 async def on_message(message_in):
-    await client.send_message(message_in.author, message_in.content)
-    await client.delete_message(message_in)
+    if message_in.channel.id == "334043962094387201":
+        await client.send_message(message_in.author, message_in.content)
+        await client.delete_message(message_in)
 
 
 client.run(TOKENS.LINKBOT_TOKEN, bot=True)
