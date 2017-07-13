45bb3736441193f3a572643945a1b3ffbd23a133
Zenith00 <z@zenith.dev>
2017-07-13 16:59:52 -0700

    Fix mess importing

--- linkbot.py
+++ linkbot.py
@@ -8,7 +8,7 @@ client = discord.Client()
 @client.event
 async def on_message(message_in):
     if message_in.channel.id == "334043962094387201":
-        await client.send_message(message_in.author, message_in.content)
+        await client.send_message(message_in.author, message_in.content, embed=message_in.embed)
         await client.delete_message(message_in)
 
 
