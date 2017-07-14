bb64088e7c65865b21bf0db015f664a83cdb6965
Zenith00 <z@zenith.dev>
2017-07-13 17:36:52 -0700

    Fix mess importing

--- linkbot.py
+++ linkbot.py
@@ -14,7 +14,7 @@ async def on_message(message_in):
     try:
         if message_in.channel.id == "334043962094387201":
             if len(message_in.embeds) > 0 and message_in.content:
-                await client.send_message(message_in.author, message_in.content, embed=discord.Embed.from_data(message_in.embeds[0]))
+                await client.send_message(message_in.author, message_in.content, embed=discord.Embed().from_data(message_in.embeds[0]))
             elif len(message_in.embeds) > 0:
                 await client.send_message(message_in.author, embed=message_in.embeds[0])
             elif message_in.content:
