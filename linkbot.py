c6459c7b2eb8b16298636a46433af95eda090269
Zenith00 <z@zenith.dev>
2017-07-13 17:23:13 -0700

    Fix mess importing

--- linkbot.py
+++ linkbot.py
@@ -1,3 +1,5 @@
+import traceback
+
 import discord
 import TOKENS
 
@@ -7,15 +9,18 @@ client = discord.Client()
 
 @client.event
 async def on_message(message_in):
-    if message_in.channel.id == "334043962094387201":
-        if message_in.embed and message_in.content:
-            await client.send_message(message_in.author, message_in.content, embed=message_in.embed)
-        elif message_in.embed:
-            await client.send_message(message_in.author, embed=message_in.embed)
-        elif message_in.content:
-            await client.send_message(message_in.author, message_in.content)
+    try:
+        if message_in.channel.id == "334043962094387201":
+            if message_in.embed and message_in.content:
+                await client.send_message(message_in.author, message_in.content, embed=message_in.embed)
+            elif message_in.embed:
+                await client.send_message(message_in.author, embed=message_in.embed)
+            elif message_in.content:
+                await client.send_message(message_in.author, message_in.content)
 
-        await client.delete_message(message_in)
+            await client.delete_message(message_in)
+    except:
+        print(traceback.format_exc())
 
 
 client.run(TOKENS.LINKBOT_TOKEN, bot=True)
