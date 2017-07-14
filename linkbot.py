fd323308ea6f503857f8dbfc9c12efdf66963e6f
Zenith00 <z@zenith.dev>
2017-07-13 17:26:12 -0700

    Fix mess importing

--- linkbot.py
+++ linkbot.py
@@ -21,6 +21,19 @@ async def on_message(message_in):
             await client.delete_message(message_in)
     except:
         print(traceback.format_exc())
+class Unbuffered(object):
+    def __init__(self, stream):
+        self.stream = stream
 
+    def write(self, data):
+        self.stream.write(data)
+        self.stream.flush()
+
+    def __getattr__(self, attr):
+        return getattr(self.stream, attr)
+
+import sys
+
+sys.stdout = Unbuffered(sys.stdout)
 
 client.run(TOKENS.LINKBOT_TOKEN, bot=True)
