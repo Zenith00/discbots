29e2143493b849ee896923c8acfafd30dfc8d901
Zenith00 <z@zenith.dev>
2018-01-09 15:45:52 -0800

    Update

--- selfbot.py
+++ selfbot.py
@@ -1492,7 +1492,7 @@ async def update_trusted_data(start, end):
                 print(trusted_id)
                 trusted = await count_trusted(trusted_id)
                 print("Adding Trusted Count: " + str(trusted))
-                new_row += str(trusted)
+                new_row.append(str(trusted))
                 print("NEW ROW")
                 print(len(new_row))
                 print(new_row)
@@ -1500,7 +1500,7 @@ async def update_trusted_data(start, end):
 
                 non_trusted = await count_non_trusted(trusted_id)
                 print("Adding NonTrusted Count: " + str(non_trusted))
-                new_row_non += str(non_trusted)
+                new_row_non.append(str(non_trusted))
                 print("NEW ROW NON TRUSTED")
                 print(len(new_row_non))
                 print(new_row_non)
