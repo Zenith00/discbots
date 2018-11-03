0b4119e6455bc50b4eaffb25532fdc6ee211d3a2
Zenith00 <z@zenith.dev>
2018-11-03 16:38:23 -0500

    Cleanup

--- rewrite/command/commands/query.py
+++
@@ -1,7 +0,0 @@
-from utils import magic
-
-def run(type="user", target="default"):
-    if target == "default":
-        target = magic.get_from_first_parent("client")
-        print(target)
-        print(target.user.id)
--- rewrite/utils/magic.py
+++
@@ -1,8 +0,0 @@
-import inspect
-
-
-def get_from_first_parent(key):
-    try:
-        return dict(inspect.getmembers(inspect.stack()[-1][0]))["f_globals"][key]
-    except KeyError:
-        return dict(inspect.getmembers(inspect.stack()[-1][0]))["f_locals"][key]
