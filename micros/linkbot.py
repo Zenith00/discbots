43b8ca7c3579f02eee71275841a6d58bcdc84d89
Zenith00 <z@zenith.dev>
2018-12-25 14:53:43 -0800

    Cleanup

--- micros/linkbot.py
+++ micros/linkbot.py
@@ -2,7 +2,7 @@ import logging
 import traceback
 
 import discord
-import TOKENS
+from .. import TOKENS
 
 
 client = discord.Client()
@@ -12,15 +12,15 @@ logging.basicConfig(level=logging.INFO)
 @client.event
 async def on_message(message_in):
     try:
-        if message_in.channel.id == "334043962094387201":
+        if message_in.channel.id in [334043962094387201, 527254103764500493]:
             if len(message_in.embeds) > 0 and message_in.content:
-                await client.send_message(message_in.author, message_in.content, embed=discord.Embed().from_data(message_in.embeds[0]))
+                await message_in.author.send(message_in.author, message_in.content, embed=discord.Embed().from_data(message_in.embeds[0]))
             elif len(message_in.embeds) > 0:
-                await client.send_message(message_in.author, embed=discord.Embed().from_data(message_in.embeds[0]))
+                await message_in.author.send(message_in.author, embed=discord.Embed().from_data(message_in.embeds[0]))
             elif message_in.content:
-                await client.send_message(message_in.author, message_in.content)
+                await message_in.author.send(message_in.author, message_in.content)
 
-            await client.delete_message(message_in)
+            await message_in.delete()
     except:
         print(traceback.format_exc())
 
