c9e4acde8479a68ec881929be4a3537761fd9cae
Zenith00 <z@zenith.dev>
2019-01-18 22:04:24 -0600

    fix

--- CONSTANTS.py
+++ CONSTANTS.py
@@ -1,10 +1,10 @@
 PINBOT = {"COMMAND_HELP":
                 [["Command", "Params", "Description", "Note"],
-                 ["set", "[name] [value]", "Sets config [name] to be [value]", "Dangerous if used on PINMAP, ALLOWED_IDS"],
-                 ["whitelist", "[role/member]", "Toggles whitelist of role/member to use commands", ""],
+                 ["set", "[name] [value]", "Sets config [name] to be [value]", "Dangerous if used on PINMAP, ALLOWED_IDS"],                           ["unset", "[name]", "Restores config [name] to default", "In case of emergency, break glass"],
+                 ["whitelist", "[role/member]", "Toggles whitelist of role/member to use commands", "Try a mention"],
                  ["print", "", "Prints current config", ""],
-                 ["map", "[from-channel] [to-channel]", "Saves pins from [from-channel] to [to-channel]", ""],
-                 ["unmap", "[from-channel]", "Stops mapping pins from [from-channel]", ""],
+                 ["map", "[from-channel] [to-channel]", "Saves pins from [from-channel] to [to-channel]", "many-to-one"],
+                 ["unmap", "[from-channel]", "Stops saving pins from [from-channel]", ""],
                  ]}
 
 
