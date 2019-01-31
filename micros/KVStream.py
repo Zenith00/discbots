951fabe19a289252160864d4b991a07dc55e58e7
Zenith00 <z@zenith.dev>
2019-01-31 08:28:45 -0600

    Debug async integration (+5 squashed commit)
    Squashed commit:
    
    [b05c447] catch all reddits for testing
    
    [cd4a57b] catch all reddits for testing
    
    [87820ed] update import
    
    [b7a2997] update import
    
    [9ae3b55] update import

--- micros/KVStream.py
+++ micros/KVStream.py
@@ -1,25 +1,37 @@
 import asyncio
+import datetime
+import logging
+import traceback
+
 import discord
 import praw
 from praw import models as praw_models
 import lux
 import TOKENS
-redd = praw.Reddit(client_id=TOKENS.REDDIT_ID, client_secret=TOKENS.REDDIT_SECRET, user_agent="KVStream")
-
-@asyncio.coroutine
-async def astream():
-    subreddit_stream = redd.subreddit("KindVoices").stream.submissions()
-    while True:
-        submission = yield subreddit_stream #type: praw_models.Submission
-        embed = discord.Embed()
-        embed.set_author(name="/u/"+submission.author.name, icon_url=submission.author.icon_img,
-                         url=f"https://www.reddit.com/u/{submission.author.name}")
-        embed.description = lux.zutils.threshold_string(submission.selftext, 1000)
-        embed.set_footer(text=f"Submitted at {submission.created_utc}")
-        await client.get_channel(540332172670926851).send(content=submission.shortlink, embed=embed)
+logging.basicConfig(level=logging.INFO)
 
 CONFIG = lux.config.Config(botname="KVSRSTREAM").load()
 client = lux.client.Lux(CONFIG)
-client.loop.run_until_complete(astream())
 
+redd = praw.Reddit(client_id=TOKENS.REDDIT_ID, client_secret=TOKENS.REDDIT_SECRET, user_agent="KVStream")
+
+
+
+@asyncio.coroutine
+async def astream():
+    await client.wait_until_ready()
+    for submission in redd.subreddit("kindvoice").stream.submissions():
+        try:
+            print("Yielding...?", flush=True)
+            embed = discord.Embed()
+            embed.set_author(name="/u/"+submission.author.name, icon_url=submission.author.icon_img,
+                             url=f"https://www.reddit.com/u/{submission.author.name}")
+            embed.description = lux.zutils.threshold_string(submission.selftext, 1000)
+            embed.set_footer(text=f"Submitted at {datetime.datetime.utcfromtimestamp(submission.created_utc).isoformat(' ')}")
+            channel = client.get_channel(540332172670926851)
+            await channel.send(content=submission.shortlink, embed=embed)
+        except:
+            print(traceback.format_exc())
+
+client.loop.create_task(astream())
 client.run(CONFIG.TOKEN)
