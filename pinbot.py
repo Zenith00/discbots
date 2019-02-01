92f08b553859923a9ba9f6accf168b3d7db8dfc7
Zenith00 <z@zenith.dev>
2019-02-01 16:22:53 -0600

    Fix

--- pinbot.py
+++ pinbot.py
@@ -136,6 +136,10 @@ async def on_message_edit(message_bef: discord.Message, message_aft: discord.Mes
 async def on_ready():
     await client.change_presence(activity=discord.Game(name="pinbot.page.link/invite for support"))
 
+@client.event
+async def on_resumed():
+    await client.change_presence(activity=discord.Game(name="pinbot.page.link/invite for support"))
+
 async def process_pin(ctx: lux.contexter.Contexter):
     channel_pins = await ctx.m.channel.pins()
     if len(channel_pins) > ctx.config["PIN_THRESHOLD"]:
