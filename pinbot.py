913ae543a4a73652dccab031a0406175357ba5b4
Zenith00 <z@zenith.dev>
2019-02-02 19:45:14 -0600

    Change presence

--- pinbot.py
+++ pinbot.py
@@ -139,11 +139,11 @@ async def on_message_edit(message_bef: discord.Message, message_aft: discord.Mes
 
 @client.event
 async def on_ready():
-    await client.change_presence(activity=discord.Game(name="Configs have been accidentally reset\npinbot.page.link/invite for support"))
+    await client.change_presence(activity=discord.Game(name="pinbot.page.link/invite for support"))
 
 @client.event
 async def on_resumed():
-    await client.change_presence(activity=discord.Game(name="Configs have been accidentally reset\npinbot.page.link/invite for support"))
+    await client.change_presence(activity=discord.Game(name="pinbot.page.link/invite for support"))
 
 async def process_pin(ctx: lux.contexter.Contexter):
     channel_pins = await ctx.m.channel.pins()
