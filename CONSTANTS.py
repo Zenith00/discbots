7952ba0ebd1aaec31f12794d62c30b79b60120ee
Zenith00 <z@zenith.dev>
2019-07-31 10:37:38 -0700

    a

--- CONSTANTS.py
+++ CONSTANTS.py
@@ -5,16 +5,16 @@ PINBOT = {"COMMAND_HELP":
                ["setup", "", "", "Help with setting up & Common problems"],
 
                ["setmax", "[#|max pins]", "Sets the max # of pins before embedding", "Short for ,,config set PIN_THRESHOLD [#]"],
-               ["map", "[#from] [#to]", "Saves pins from [#from] to [#to]", "many-to-one"],
+               ["map", "[#from-channel] [#to-channel]", "Saves pins from [#from-channel] to [#to-channel]", "many-to-one"],
                ["unmap", "[#from]", "Stops saving pins from [from-channel]", ""],
                ["whitelist", "[@|role/member]", "Allow/Forbid role/member using commands", "Try a mention?"],
                ["setprefix", "[prefix]", "Sets prefix to [prefix]", ""],
                ["", "", "", ""],
-               ["config set", "[name] [val]", "Sets config [name] to leval([val])", "With great power..."],
+               ["config set", "[name] [val]", "Sets config [name] to literal_eval([val])", "With great power..."],
                ["config unset", "[name]", "Restores config [name] to default", "...comes great responsibility"],
                ["config reset", "", "Restores all config to default", "In case of emergency, break glass"],
                ["config print", "", "Prints current config", "Secret settings pending documentation..."],
                ["", "", "", ""],
-               ["pinall", "", "Embeds current channel until < max", ""],
+               ["pinall", "", "Manually embeds current channel until pins < max", ""],
 
                ]}
