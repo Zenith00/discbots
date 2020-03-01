24c65b4849076a069bc1a9b381c9bbc2e22f7886
Zenith00 <zenith00dev@gmail.com>
2020-03-01 03:39:29 -0600

    blah

--- micros/statusbot.py
+++ micros/statusbot.py
@@ -2,22 +2,20 @@ import discord
 import TOKENS
 import mcstatus
 import asyncio
+
 client = discord.Client()
-
-
 
 async def clock():
     await client.wait_until_ready()
     while not client.is_closed():
-        await client.change_presence(
-            activity=discord.Game(name=f"{mcstatus.MinecraftServer.lookup('owowatch.ardittristan.xyz').status().players.online} users online")
-        )
-        await asyncio.sleep(30)
+        try:
 
-
+            await client.change_presence(
+                activity=discord.Game(name=f"{mcstatus.MinecraftServer.lookup('owowatch.ardittristan.xyz').status().players.online} users online")
+            )
+            await asyncio.sleep(30)
+        except:
+            pass
 
 client.loop.create_task(clock())
-
-
-
 client.run(TOKENS.STATUSBOT_TOKEN, bot=True)
